0c8788eb8cc13aac8a9b7fa1f5a7306f4e983639
not-just-tom <111307154+not-just-tom@users.noreply.github.com>
2026-04-02 15:07:34 +0100

    Fix command to run Snakemake with Python module

--- shotton/run.sh
+++ shotton/run.sh
@@ -18,7 +18,7 @@ conda activate pypsa
 cd $SLURM_SUBMIT_DIR
 
 # Run 
-snakemake -s Snakefile -j 1 solve_all_networks \
+python -m snakemake -s Snakefile -j 1 solve_all_networks \
   --rerun-incomplete \
   --latency-wait 60 \
   --printshellcmds
