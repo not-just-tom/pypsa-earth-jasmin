c46e57709992a548c2005691a591aad89d82a956
not-just-tom <111307154+not-just-tom@users.noreply.github.com>
2026-04-03 15:48:44 +0100

    Fix typo in config file name in run.sh

--- shotton/run.sh
+++ shotton/run.sh
@@ -19,7 +19,7 @@ cd $SLURM_SUBMIT_DIR
 
 # Run 
 python -m snakemake -s Snakefile -j 1 solve_all_networks \
-  --config congfig_cn.yaml
+  --config config_cn.yaml
   --rerun-incomplete \
   --latency-wait 60 \
   --printshellcmds
