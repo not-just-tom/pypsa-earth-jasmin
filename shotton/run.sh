8374ffe28b10e034d4c95a1a81931a93a14e9ae7
not-just-tom <111307154+not-just-tom@users.noreply.github.com>
2026-04-03 16:15:08 +0100

    Update run.sh

--- shotton/run.sh
+++ shotton/run.sh
@@ -19,7 +19,6 @@ cd $SLURM_SUBMIT_DIR
 
 # Run 
 python -m snakemake -s Snakefile -j 1 solve_all_networks \
-  --configfile config_cn.yaml
   --rerun-incomplete \
   --latency-wait 60 \
   --printshellcmds
