ac5c86e92a2918017a82fe087dba578faae94f8f
not-just-tom <111307154+not-just-tom@users.noreply.github.com>
2026-04-13 09:25:43 +0100

    Add SLURM script for GPU job execution

---
+++ shotton/run_gpu.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --account=gbov
+#SBATCH --job-name=china_run
+#SBATCH --partition=orchid
+#SBATCH --qos=orchid
+#SBATCH --gres:gpu:1
+#SBATCH --nodes=1
+#SBATCH --time=2-00:00:00
+#SBATCH --mem=64G
+#SBATCH -o logs/slurm-%j.out
+#SBATCH -e logs/slurm-%j.err
+
+# Load modules
+module unload jaspy
+source ~/miniforge3/bin/activate
+conda activate pypsa
+
+# Change to repository root
+cd $SLURM_SUBMIT_DIR
+
+# Run 
+python -m snakemake -s Snakefile -j 1 solve_all_networks \
+  --rerun-incomplete \
+  --latency-wait 60 \
+  --printshellcmds
+
+
+
