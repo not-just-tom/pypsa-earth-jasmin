1598f978d5a20f935e3ba7c03bb6188eb729fabe
not-just-tom <111307154+not-just-tom@users.noreply.github.com>
2026-05-02 15:52:17 +0100

    Update run.sh

--- shotton/run.sh
+++ shotton/run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #SBATCH --account=gbov
-#SBATCH --job-name=china_run
+#SBATCH --job-name=pypsa_country
 #SBATCH --partition=orchid
+#SBATCH --account=orchid
 #SBATCH --qos=orchid
-#SBATCH --nodes=1
-#SBATCH --time=2-00:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --time=24:00:00
 #SBATCH --mem=64G
 #SBATCH -o logs/slurm-%j.out
 #SBATCH -e logs/slurm-%j.err
